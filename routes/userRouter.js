c0b54ad7c8a919b749dc2ee9111c91442ffb1117
polinamln <polina77979.la@gmail.com>
2024-05-23 21:12:26 +0200

    1

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,16 +1,19 @@
 import express from "express";
 import {
   userLogin,
-  userRegistratoin,
+  userRegistration,
   userLogout,
   userCurrent,
   userSubscription,
+  userAvatar,
+  getAvatar,
 } from "../controllers/userControllers.js";
 import authMiddleware from "../middleware/auth.js";
+import uploadMiddleware from "../middleware/upload.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userRegistratoin);
+userRouter.post("/register", userRegistration);
 
 userRouter.post("/login", userLogin);
 
@@ -19,5 +22,14 @@ userRouter.post("/logout", authMiddleware, userLogout);
 userRouter.get("/current", authMiddleware, userCurrent);
 
 userRouter.patch("/", authMiddleware, userSubscription);
+
+userRouter.patch(
+  "/avatars",
+  uploadMiddleware.single("avatar"),
+  authMiddleware,
+  userAvatar
+);
+
+userRouter.get("/avatars/:fileName", authMiddleware, getAvatar);
 
 export default userRouter;
--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -3,4 +3,5 @@ import Joi from "joi";
 export const createUserSchema = Joi.object({
   email: Joi.required(),
   password: Joi.required(),
+  avatarURL: Joi.optional(),
 });
