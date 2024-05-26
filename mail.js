fb3e04946d0fe5c4656722c8c73c081015131edf
polinamln <polina77979.la@gmail.com>
2024-05-26 22:41:20 +0200

    1

---
+++ mail.js
@@ -0,0 +1,15 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USERNAME,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+
+export const userMail = (message) => {
+  return transport.sendMail(message);
+};
--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -4,4 +4,9 @@ export const createUserSchema = Joi.object({
   email: Joi.required(),
   password: Joi.required(),
   avatarURL: Joi.optional(),
+  verificationToken: Joi.optional(),
+});
+
+export const resendVerificationEmailUserSchema = Joi.object({
+  email: Joi.string().required(),
 });
